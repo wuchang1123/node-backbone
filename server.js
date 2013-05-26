269a3324b4bdf74f530dd1c8d5f3115fd3380d06
wuchang1123 <wuchang1123@163.com>
2013-05-26 18:07:02 +0800

    update

--- server.js
+++ server.js
@@ -9,19 +9,30 @@ app.use(function(req, res, next){
   next();
 });
 
+// less 请求
 app.get(/\/assets\/css\/(.*)\.(css|less)$/, function(req, res, next) {
 	var file = req.params[0];
 	var paths = file.split(/[\/\\]+/);
 	lessp.apply(null, [res].concat(paths));
 });
 
+// api 服务
 app.get(/\/api\/(.*)\.(json)$/, function(req, res, next) {
-	var file = req.params[0];
+	var file = req.params[0].replace(/\.\.+/g, "");
 	var paths = file.split(/[\/\\]+/);
 	res.type('application/json; charset=utf-8');
 	res.send(loadJson.apply(null, [res].concat(paths)));
 });
 
+// package.json
+app.get(/package\.json$/, function(req, res, next) {
+	var paths = ["..", "..", "package"];
+	res.type('application/json; charset=utf-8');
+	res.send(loadJson.apply(null, [res].concat(paths)));
+});
+
+
+// 静态文件服务
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
